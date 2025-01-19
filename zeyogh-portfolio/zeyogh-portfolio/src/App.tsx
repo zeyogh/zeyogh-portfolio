e530f54d5ee81161fcdd9393192cf72acd37c613
zeyogh <zeynep.ogh@gmail.com>
2025-01-18 18:18:28 -0600

    react three fiber spinning shaded cubes

--- zeyogh-portfolio/zeyogh-portfolio/src/App.tsx
+++ zeyogh-portfolio/zeyogh-portfolio/src/App.tsx
@@ -1,12 +1,38 @@
-import styles from './App.module.css'
+import * as THREE from 'three'
+import { useRef, useState } from 'react'
+import { Canvas, useFrame } from '@react-three/fiber'
 
-function App() {
+function Box(props: JSX.IntrinsicElements['mesh']) {
+  // This reference will give us direct access to the THREE.Mesh object
+  const ref = useRef<THREE.Mesh>(null!)
+  // Hold state for hovered and clicked events
+  const [hovered, hover] = useState(false)
+  const [clicked, click] = useState(false)
+  // Rotate mesh every frame, this is outside of React without overhead
+  useFrame((state, delta) => (ref.current.rotation.x += 0.01))
 
   return (
-    <div className={styles.App}>
-      Hello, World!
-    </div>
+    <mesh
+      {...props}
+      castShadow receiveShadow
+      ref={ref}
+      scale={clicked ? 1.5 : 1}
+      onClick={(event) => click(!clicked)}
+      onPointerOver={(event) => hover(true)}
+      onPointerOut={(event) => hover(false)}>
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={hovered ? 'hotpink' : 'blue'} />
+    </mesh>
   )
 }
 
-export default App
+export default function App() {
+  return (
+    <Canvas>
+      <ambientLight intensity={0.5} />
+      <directionalLight position={[5, 5, 5]} intensity={1} />
+      <Box position={[-1.2, 0, 0]} />
+      <Box position={[1.2, 0, 0]} />
+    </Canvas>
+  )
+}
